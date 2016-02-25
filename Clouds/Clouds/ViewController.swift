49aefcbc1a18842051bd745dd9287cef554c551a
Kevia <cloud@kevia.me>
2016-02-24 17:06:33 -0800

    Start of project

---
+++ Clouds/Clouds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clouds
+//
+//  Created by jhampac on 2/24/16.
+//  Copyright © 2016 jhampac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
