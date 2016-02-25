d87969effd39f945b080e2e809bdb0d6ea95d2cf
Kevia <cloud@kevia.me>
2016-02-25 15:11:07 -0800

    iCloud downloading records complete

--- Clouds/Clouds/ViewController.swift
+++ Clouds/Clouds/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 {
     var tableView: UITableView!
     var whistles = [Whistle]()
-    static var dirty = false
+    static var dirty = true
     
     // MARK: - VC LifeCyle
     
@@ -41,6 +41,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         title = "What is that hummm"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "addWhistle")
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Home", style: .Plain, target: nil, action: nil)
+        
+        tableView.registerClass(UITableViewCell.self, forHeaderFooterViewReuseIdentifier: "Cell")
     }
     
     override func viewWillAppear(animated: Bool)
@@ -68,7 +70,44 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func loadWhistles()
     {
+        let pred = NSPredicate(value: true)
+        let sort = NSSortDescriptor(key: "creationDate", ascending: false)
+        let query = CKQuery(recordType: "Whistles", predicate: pred)
+        query.sortDescriptors = [sort]
         
+        let operation = CKQueryOperation(query: query)
+        operation.desiredKeys = ["genre", "comments"]
+        operation.resultsLimit = 50
+        
+        var newWhistles = [Whistle]()
+        
+        // Give operation object property a closure to do
+        operation.recordFetchedBlock = { (record) in
+            let whistle = Whistle()
+            whistle.recordID = record.recordID
+            whistle.genre = record["genre"] as! String
+            whistle.comments = record["comments"] as! String
+            newWhistles.append(whistle)
+        }
+        
+        operation.queryCompletionBlock = { [unowned self] (cursor, error) -> Void in
+            dispatch_async(dispatch_get_main_queue()) {
+                if error == nil
+                {
+                    ViewController.dirty = false
+                    self.whistles = newWhistles
+                    self.tableView.reloadData()
+                }
+                else
+                {
+                    let ac = UIAlertController(title: "Fetch failed", message: "There was a problem fetching the list of whistles; please try again: \(error!.localizedDescription)", preferredStyle: .Alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                    self.presentViewController(ac, animated: true, completion: nil)
+                }
+            }
+        }
+        
+        CKContainer.defaultContainer().publicCloudDatabase.addOperation(operation)
     }
     
     func makeAttributedString(title title: String, subtitle: String) -> NSAttributedString
@@ -84,6 +123,35 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
         
         return titleString
+    }
+    
+    // MARK: - TableView Callbacks
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return whistles.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        cell.accessoryType = .DisclosureIndicator
+        cell.textLabel?.attributedText = makeAttributedString(title: whistles[indexPath.row].genre, subtitle: whistles[indexPath.row].comments)
+        cell.textLabel?.numberOfLines = 0
+        return cell
+    }
+    
+    // Make cell heights dynamic
+    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
     }
 }
 
