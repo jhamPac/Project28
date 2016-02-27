b1cc841d8218d980bca4ac14707e44b4c25278ab
Kevia <cloud@kevia.me>
2016-02-26 16:37:53 -0800

    Phew finishing up ResultsViewController so it downloads audio from iCloud

--- Clouds/Clouds/ResultsViewController.swift
+++ Clouds/Clouds/ResultsViewController.swift
@@ -20,36 +20,135 @@ class ResultsViewController: UITableViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
+        
+        title = "Genre: \(whistle.genre)"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Download", style: .Plain, target: self, action: "downloadTapped")
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        let reference = CKReference(recordID: whistle.recordID, action: .DeleteSelf)
+        let pred = NSPredicate(format: "owningWhistle == %@", argumentArray: [reference])
+        let sort = NSSortDescriptor(key: "creationDate", ascending: true)
+        let query = CKQuery(recordType: "Suggestions", predicate: pred)
+        query.sortDescriptors = [sort]
+        
+        CKContainer.defaultContainer().publicCloudDatabase.performQuery(query, inZoneWithID: nil) { [unowned self] (results, error) -> Void in
+            
+            guard error == nil else { print(error!.localizedDescription); return }
+            
+            if let results = results
+            {
+                self.parseResults(results)
+            }
+        }
+    }
+    
+    // MARK: - VC Methods
+    
+    func addSuggestion(suggest: String)
+    {
+        let suggestionRecord = CKRecord(recordType: "Suggestions")
+        let reference = CKReference(recordID: whistle.recordID, action: .DeleteSelf)
+        suggestionRecord["text"] = suggest
+        suggestionRecord["owningWhistle"] = reference
+        
+        // send to iCloud
+        CKContainer.defaultContainer().publicCloudDatabase.saveRecord(suggestionRecord) { [unowned self] (record, error) -> Void in
+            dispatch_async(dispatch_get_main_queue()) {
+                if error == nil
+                {
+                    self.suggestions.append(suggest)
+                    self.tableView.reloadData()
+                }
+                else
+                {
+                    let ac = UIAlertController(title: "Error", message: "There was a problem submitting your suggestion: \(error!.localizedDescription)", preferredStyle: .Alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                    self.presentViewController(ac, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    func parseResults(records: [CKRecord])
+    {
+        var newSuggestions = [String]()
+        
+        for record in records
+        {
+            newSuggestions.append(record["text"] as! String)
+        }
+        
+        dispatch_async(dispatch_get_main_queue()) {
+            self.suggestions = newSuggestions
+            self.tableView.reloadData()
+        }
+    }
+    
+    func downloadTapped()
+    {
+        
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-    
-    // MARK: - TableView Callbacks
-    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int
     {
-        return 2
+        return 1
     }
 
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        if section == 0
+        {
+            return 1
+        }
+        else
+        {
+            // add one for the Add Suggestion row
+            return max(1, suggestions.count + 1)
+        }
+    }
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        cell.selectionStyle = .None
+        cell.textLabel?.numberOfLines = 0
+        
+        if indexPath.section == 0
+        {
+            cell.textLabel?.font = UIFont.preferredFontForTextStyle(UIFontTextStyleTitle1)
+            
+            if whistle.comments.characters.count == 0
+            {
+                cell.textLabel?.text = "Comments: None"
+            }
+            else
+            {
+                cell.textLabel?.text = whistle.comments
+            }
+        }
+        else
+        {
+            cell.textLabel?.font = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
+            
+            if indexPath.row == suggestions.count
+            {
+                // That means this is the extra row we added; max(1, suggestions.count + 1)
+                cell.textLabel?.text = "Add suggestion"
+                cell.selectionStyle = .Gray
+            }
+            else
+            {
+                cell.textLabel?.text = suggestions[indexPath.row]
+            }
+        }
+        
         return cell
     }
+    
+    // MARK: - Table view delegates
     
     override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String?
     {
@@ -60,40 +159,40 @@ class ResultsViewController: UITableViewController
         
         return nil
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    {
+        return UITableViewAutomaticDimension
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    {
+        return UITableViewAutomaticDimension
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+    {
+        // Guard to check if its the last row
+        guard indexPath.section == 1 && indexPath.row == suggestions.count else { return }
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let ac = UIAlertController(title: "Suggest a song…", message: nil, preferredStyle: .Alert)
+        var suggestion: UITextField!
+        
+        ac.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            suggestion = textField
+            textField.autocorrectionType = .Yes
+        }
+        
+        ac.addAction(UIAlertAction(title: "Submit", style: .Default) { (action) -> Void in
+            if suggestion.text?.characters.count > 0
+            {
+                self.addSuggestion(suggestion.text!)
+            }
+        })
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
 }
